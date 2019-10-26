0fa9e768883cc3aa328776863e62d94c7ba4c462
tbhanson96 <tbh20715@gmail.com>
2019-10-26 00:01:18 -0700

    added config service test

---
+++ server/test/services/config-service.spec.ts
@@ -0,0 +1,28 @@
+jest.mock('dotenv');
+import * as dotenv from 'dotenv';
+import { ConfigService } from '../../src/services/config.service';
+import { promisify } from 'util';
+
+describe('ConfigService', () => {
+
+    let mockDotEnv = <any>dotenv;
+    beforeEach(() => {
+        jest.spyOn(mockDotEnv, 'config').mockImplementation(() => {
+            process.env = {...process.env, ROOT_DIR: '{.}/..', PORT: '6969' };
+        });
+    });
+
+    it('parses into process.env correctly', () => {
+        const service = new ConfigService('path');
+        expect(mockDotEnv.config).toBeCalled();
+        expect(service.env.ROOT_DIR).toEqual('path/..')
+        expect(service.env.PORT).toEqual('6969');
+    });
+
+    it('throws if env does not find key', () => {
+        const service = new ConfigService('path');
+        expect(() => {
+            service.env.NOT_A_KEY
+        }).toThrow();
+    });
+});
