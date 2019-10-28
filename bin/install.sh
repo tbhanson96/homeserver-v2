fc19beb7ffb713394661d0af9dd61636c00021b7
tbhanson96 <tbh20715@gmail.com>
2019-10-27 21:35:56 -0700

    always exit 1

--- bin/install.sh
+++ bin/install.sh
@@ -3,20 +3,20 @@ run_command()
     eval $1
     if [ $? -ne 0 ]
     then
-        exit $?
+        exit 1
     fi
 }
 
 SCRIPT=$(readlink -f "$BASH_SOURCE")
 DIRNAME=$(dirname "$SCRIPT")
 echo "Installing all necessary files..."
-cd $DIRNAME/../server/src
+cd $DIRNAME/../server
 echo "Building server..."
 run_command "npm install"
 run_command "npm run build:prod"
 echo "Finished building server."
 
-cd $DIRNAME/../client/src
+cd $DIRNAME/../client
 echo "Building client..."
 run_command "npm install"
 run_command "npm run build:prod"
